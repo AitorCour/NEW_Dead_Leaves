5f15a91da6dd9496710eafe9686feb128f205afc
DESKTOP-DQHGR6Q\aitor <aitorcour@gmail.com>
2020-01-23 22:14:49 +0100

    First Commit! Camera Based movement done, make for more cameras, and start to experiment with more camera types

---
+++ NEW_Dead_Leaves/Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerController playerController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputAxis = Vector2.zero;
+
+        inputAxis.x = Input.GetAxisRaw("Horizontal");
+        inputAxis.y = Input.GetAxisRaw("Vertical");
+        playerController.Move(inputAxis);
+    }
+}
+
---
+++ NEW_Dead_Leaves/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cameraDef;
+    public GameObject mesh;
+    public float speed;
+    private float timeRotate = 5;
+    private Vector2 stopped;
+    private Vector3 rightDef;
+    private Vector3 forwardDef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopped = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Move(Vector2 inputAxis)
+    {
+        if (cameraDef == null) return;
+        else if(inputAxis == stopped)
+        {
+            Vector3 right = cameraDef.transform.right;
+            Vector3 forward = Vector3.Cross(right, Vector3.up);
+            rightDef = right;
+            forwardDef = forward;
+        }
+        Vector3 movement = Vector3.zero;
+        if(inputAxis != stopped) //cuando el personaje esta en movimiento
+        {
+            movement += rightDef * (inputAxis.x * speed * Time.deltaTime);
+            movement += forwardDef * (inputAxis.y * speed * Time.deltaTime);
+        }
+        transform.Translate(movement);
+        if (movement != Vector3.zero) //Rota el personaje. se rota su mesh, no el objeto en si
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(movement);
+            mesh.transform.rotation = Quaternion.RotateTowards(mesh.transform.rotation, lookRotation, timeRotate);
+
+        }
+    }
+}
