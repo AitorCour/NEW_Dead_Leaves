77fef879b17e32dfc3c6e3fd968e676cf86947c4
DESKTOP-DQHGR6Q\aitor <aitorcour@gmail.com>
2020-01-25 13:03:55 +0100

    movimiento perfeccionado. no se da contra paredes y no va mes rapido en diagonal

--- NEW_Dead_Leaves/Assets/Scripts/PlayerController.cs
+++ NEW_Dead_Leaves/Assets/Scripts/PlayerController.cs
@@ -6,21 +6,48 @@ public class PlayerController : MonoBehaviour
 {
     public Camera cameraDef;
     public GameObject mesh;
+
     public float speed;
-    private float timeRotate = 5;
+    private float timeRotate = 10;
+    public float rayDistance;
+
+    private bool canWalk;
+    public bool canMoveForward;
+    public bool canMoveBackwards;
+    public bool canMoveRight;
+    public bool canMoveLeft;
+
     private Vector2 stopped;
+    public Vector2 actualVector;
     private Vector3 rightDef;
     private Vector3 forwardDef;
+
+    public LayerMask ground;
     // Start is called before the first frame update
     void Start()
     {
         stopped = new Vector2(0, 0);
+        canWalk = true;
+        canMoveForward = true;
     }
+    private void OnDrawGizmos()
+    {
+        Vector3 direction = mesh.transform.TransformDirection(Vector3.forward) * rayDistance;
+        Gizmos.DrawRay(mesh.transform.position, direction); //forward
 
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 direction = mesh.transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(mesh.transform.position, direction, rayDistance, ground) && canMoveForward)
+        {
+            canMoveForward = false;
+        }
+        if (!Physics.Raycast(mesh.transform.position, direction, rayDistance, ground) && !canMoveForward)
+        {
+            canMoveForward = true;
+        }
     }
     public void Move(Vector2 inputAxis)
     {
@@ -35,15 +62,20 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = Vector3.zero;
         if(inputAxis != stopped) //cuando el personaje esta en movimiento
         {
-            movement += rightDef * (inputAxis.x * speed * Time.deltaTime);
-            movement += forwardDef * (inputAxis.y * speed * Time.deltaTime);
+            inputAxis = Vector3.ClampMagnitude(inputAxis, 1f);
+
+            movement += rightDef * (inputAxis.x * Time.deltaTime);
+            movement += forwardDef * (inputAxis.y * Time.deltaTime);
+            actualVector = inputAxis;
         }
-        transform.Translate(movement);
+        if(canMoveForward)
+        {
+            transform.Translate(movement * speed);
+        }
         if (movement != Vector3.zero) //Rota el personaje. se rota su mesh, no el objeto en si
         {
             Quaternion lookRotation = Quaternion.LookRotation(movement);
             mesh.transform.rotation = Quaternion.RotateTowards(mesh.transform.rotation, lookRotation, timeRotate);
-
         }
     }
 }
